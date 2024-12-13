113c02c5b351bdeaec348f482338582e4e09703b
chanyangpraise <cychoi710@icloud.com>
2024-12-13 15:38:45 +0900

    ✨ Feat: Add Error Handling Middleware
    create switch case for errorHandler.js

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -9,7 +9,6 @@ const {
 } = require("../controllers/contactControllers");
 
 router.route("/").get(getContacts).post(createContact);
-
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
